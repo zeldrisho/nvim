a9f50b5047088f4fb1fee8d87c4e6085c736997d
giatrung2012 <trungrappar2002@gmail.com>
2022-02-09 23:00:24 +0700

    Delete on_attch function

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,18 +1,13 @@
 local lspconfig = require 'lspconfig'
-local on_attach = function(_, bufnr)
-  local opts = { noremap = true, silent = true }
-  
-end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { 'clangd', 'sumneko_lua' }
+local servers = { 'clangd', 'sumneko_lua', 'bashls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach,
     capabilities = capabilities,
   }
 end
