1c5a209ebf7782c2835185c0cc204c2695c2c5ce
giatrung2012 <trungrappar2002@gmail.com>
2022-05-01 22:09:57 +0700

    Updated

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = {"cpp", "java", "bash", "lua", "markdown"},
+    ensure_installed = {"bash", "lua", "markdown", "fish"},
     highlight = {
         enable = true, -- false will disable the whole extension
     },
