824d1140c80bbe7be5ae86f71f7defe420e6054c
giatrung2012 <trunghsg2012@gmail.com>
2022-07-02 01:19:15 +0700

    Removed clangd & jdtls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,16 +5,10 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { 'clangd', 'sumneko_lua', 'bashls' }
+local servers = { 'sumneko_lua', 'bashls' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         capabilities = capabilities,
     }
 end
 
-require'lspconfig'.jdtls.setup{
-    cmd = { 'jdtls' },
-    root_dir = function(fname)
-        return require'lspconfig'.util.root_pattern('pom.xml', 'gradle.build', '.git')(fname) or vim.fn.getcwd()
-    end
-}
